28d00254a03348211af8f224630d2e81df1bba8b
Paul Turnbaugh <pet30200@gmail.com>
2016-04-04 20:06:03 -0400

    Controller Added

---
+++ Tic Tac Toe/src/game/gameboardController.java
@@ -0,0 +1,55 @@
+package game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class gameboardController {
+
+    @FXML
+    private ImageView imgMove1;
+
+    @FXML
+    private ImageView imgMove2;
+
+    @FXML
+    private ImageView imgMove3;
+
+    @FXML
+    private ImageView imgMove4;
+
+    @FXML
+    private ImageView imgMove5;
+
+    @FXML
+    private ImageView imgMove6;
+
+    @FXML
+    private ImageView imgMove7;
+
+    @FXML
+    private ImageView imgMove8;
+
+    @FXML
+    private ImageView imgMove9;
+
+    @FXML
+    private Label lblWinner;
+
+    @FXML
+    private Button btnRestart;
+
+    @FXML
+    void restartClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setImage(MouseEvent event) {
+
+    }
+
+}
